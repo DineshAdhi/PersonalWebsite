9c3fcfc1e53802449f63033736d0aa649f6b074f
Dinesh <dinsesh10c04@gmail.com>
2018-10-11 00:04:07 +0530

    Added blogs page

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var port = process.env.PORT || 5000;
 
 app.use('/', route);
 app.use(express.static(__dirname + '/views'));
-app.use(express.static(__dirname + "/icons"));
 
 app.listen(port, function(){
     console.log("Listening to port : " + port);
