7ebb761f81a1f24db0c37768eeda4a771a21d38f
Dinesh <dinsesh10c04@gmail.com>
2018-10-11 22:04:50 +0530

    Added handlebars

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 var favicon = require('serve-favicon');
-var route = express.Router();
+var exphbs  = require('express-handlebars');
 var path = require('path');
 var port = process.env.PORT || 5000;
 
-app.use('/', route);
+app.engine('handlebars', exphbs({defaultLayout: 'index'}));
+app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/views'));
 app.use(favicon(path.join(__dirname, 'views', 'icons', 'favicon.ico')))
 
@@ -13,6 +14,6 @@ app.listen(port, function(){
     console.log("Listening to port : " + port);
 });
 
-route.get("/", function(req, res){
-        res.sendFile(__dirname + "/views/index.html");
+app.get("/", function(req, res){
+    res.render('home', {'dinesh': 'adhithya'});
 });
