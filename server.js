831ead77ffe70844d4683eb8efadd62dcfec763a
Dinesh Adhithya <dinesh.adhithya@zohocorp.com>
2019-04-24 12:15:19 +0530

    made changes in background image

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var exphbs  = require('express-handlebars');
 var path = require('path');
 var port = process.env.PORT || 5000;
+var no_images = 3;
 
 var json = { 
     "seeingtheunseeable" : "https://dineshthoughts.wordpress.com/2019/04/12/seeing-the-unseeable/",
@@ -19,7 +20,8 @@ var json = {
     "divinevsdosa" : "https://dineshthoughts.wordpress.com/2017/08/15/divine-vs-dhosa/",
     "tenmonthslater": "https://medium.com/@dinesh10c04/10-months-later-ea158bf7d818",
     "gameofcolor" : "https://medium.com/@dinesh10c04/the-game-of-colour-6c4700aeb3a8",
-    "tomatoterrorist" : "https://dineshthoughts.wordpress.com/2019/04/15/the-tomato-terrorist-the-lok-sabha/"
+    "tomatoterrorist" : "https://dineshthoughts.wordpress.com/2019/04/15/the-tomato-terrorist-the-lok-sabha/",
+    "profileimage" : "/images/profileimage.jpg"
 }
 
 app.engine('handlebars', exphbs({defaultLayout: 'index'}));
@@ -32,6 +34,8 @@ app.listen(port, function(){
 });
 
 app.get("/", function(req, res){
+    var rand = Math.floor(Math.random() * (no_images - 1) + 1);
+    json['profileimage'] = "/images/profileimage" + rand + ".jpg";
     res.render('home', json);
 });
 
